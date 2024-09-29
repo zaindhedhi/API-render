30e4e8fabbae3e9e978584eded4ce129b950ca45
zaindhedhi <zaindhedhi@oulook.com>
2024-09-29 12:34:01 +0500

    update

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 let div = document.querySelector('#render')
 let index = 0
 
+function render(res){
+    if(index < res.length){
+        div.innerHTML = `
+        <h1>Employee Details</h1>
+        <p> <b>Name</b> : ${res[index].name}</p>
+        <p> <b>Email</b> : ${res[index].email}</p>
+        <p> <b>Address</b> : ${res[index].address.city}</p>
+        <h5> <b>Phone No</b> : ${res[index].phone}</p>
+        <button class="rounded-full " onclick="decrease()">Previous</button>
+        <button class="rounded-full " onclick="increase()">Next</button>
+        `
+    }
+    else{
+        div.innerHTML = `<h1>No Employees Found.</h1><br>        
+        <button onclick ="decrease()">Previous</button>`
+    }
+     
+}
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(res => res.json())
 .then(res => {
-
-
+    
+    render(res)
+   
     console.log(res)
     
-    function render(){
-        if(index < res.length){
-            div.innerHTML = `
-            <h1>Employee Details</h1>
-            <p> <b>Name</b> : ${res[index].name}</p>
-            <p> <b>Email</b> : ${res[index].email}</p>
-            <p> <b>Address</b> : ${res[index].address.city}</p>
-            <h5> <b>Phone No</b> : ${res[index].phone}</p>
-            <button class="rounded-full " onclick="decrease()">Previous</button>
-            <button class="rounded-full " onclick="increase()">Next</button>
-            `
-        }
-        else{
-            div.innerHTML = `<h1>No Employees Found.</h1><br>        
-            <button onclick ="decrease()">Previous</button>`
-        }
-         
-    }
-render()
+    
+    
     
 })
 .catch(err => console.log(err))
-
 function increase(){
     index += 1
     console.log(index)
-    render()
+    render(res)
 }
 
 function decrease(){
@@ -44,6 +45,6 @@ function decrease(){
     else{
         index -= 1
         console.log(index)
-        render()
+        render(res)
     }
 }
